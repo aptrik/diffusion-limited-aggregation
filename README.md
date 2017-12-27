1dea19608bd7b9d36b06a75c8539f45a1f252142
Patrik Berglund <aptrik@users.noreply.github.com>
2017-12-27 15:31:34 +0100

    First version.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Diffusion-limited aggregation
+
+Inspired by: [Festive Fractals - Computerphile](https://www.youtube.com/watch?v=TU3IQRV6LV0).
+
+For background read the [wikipedia article](https://en.wikipedia.org/wiki/Diffusion-limited_aggregation).
+
+![](./screenshot.png)
+
+[Try it!](https://aptrik.github.io/diffusion-limited-aggregation/).
+
+## License
+Copyright (c) 2017 Patrik Berglund.
---
+++ simulation.js
@@ -0,0 +1,161 @@
+/* jshint esversion: 6 */
+/* jshint -W097 */
+'use strict';
+
+/* global console, performance */
+
+function _random(maximum) {
+    return Math.floor(Math.random() * maximum);
+}
+
+function Simulation(width, height, ratio) {
+    this.width = width;
+    this.height = height;
+    this.ratio = Math.min(1.0, Math.max(0.0, ratio));
+    this.numberOfParticles = Math.floor(this.ratio * width * height);
+    this.steps = 0;
+    console.log("Simulation=%s", JSON.stringify(this, null, 2));
+
+    this.grid = new Array(this.width * this.height);
+    for (var i = 0; i < this.grid.length; i++) {
+        this.grid[i] = 0;
+    }
+
+    this.particles = new Array(this.numberOfParticles - 1);
+    this.stuckParticles = [];
+
+    // Stick the first particle to the grid center.
+    var x = Math.floor(this.width / 2);
+    var y = Math.floor(this.height / 2);
+    this.stuckParticles.push({x: x, y: y});
+    this.grid[this.gridIndex(x, y)] = 1;
+
+    // Place the rest of the particles randomly on the grid.
+    for (var j = 0; j < this.particles.length; j++) {
+        this.particles[j] = this.randomPosition();
+    }
+}
+
+Simulation.prototype.gridIndex = function (x, y) {
+    return x + y * this.width;
+};
+
+Simulation.prototype.randomPosition = function () {
+    var x, y;
+    do {
+        x = _random(this.width);
+        y = _random(this.height);
+    } while (this.grid[this.gridIndex(x, y)] > 0);
+    return {x: x, y: y};
+};
+
+Simulation.prototype.isParticleFree = function (position) {
+    var x = position.x;
+    var leftX = x - 1;
+    var rightX = x + 1;
+    var y = position.y;
+    var topY = y - 1;
+    var bottomY = y + 1;
+    if (x <= 0 || x >= this.width ||
+        leftX <= 0 || leftX >= this.width ||
+        rightX <= 0 || rightX >= this.width ||
+        y <= 0 || y >= this.height ||
+        topY <= 0 || topY >= this.height ||
+        bottomY <= 0 || bottomY >= this.height) {
+        return true;
+    }
+    y *= this.width;
+    topY *= this.width;
+    bottomY *= this.width;
+    if (this.grid[x + topY] ||
+        this.grid[leftX + y] ||
+        this.grid[rightX + y] ||
+        this.grid[x + bottomY] ||
+        this.grid[leftX + topY] ||
+        this.grid[leftX + bottomY] ||
+        this.grid[rightX + topY] ||
+        this.grid[rightX + bottomY]) {
+        return false;
+    }
+    return true;
+};
+
+Simulation.prototype.move = function (position) {
+    var x = position.x;
+    var y = position.y;
+    x += Math.random() > 0.5 ? 1 : -1;
+    y += Math.random() > 0.5 ? 1 : -1;
+    if ((x < 0) || (y < 0) || (x >= this.width) || (y >= this.height)) {
+        return this.randomPosition();
+    }
+    // if (x < 0) {
+    //     x += 2;
+    // } else if (x >= this.width) {
+    //     x -= 2;
+    // }
+    // if (y < 0) {
+    //     y += 2;
+    // } else if (y >= this.height) {
+    //     y -= 2;
+    // }
+    return {
+        x: x,
+        y: y
+    };
+};
+
+Simulation.prototype.step = function (steps = 1) {
+    if (!this.startTime) {
+        this.startTime = performance.now();
+    }
+    for (var step = 0; step < steps; step++) {
+        this.steps += 1;
+        var freeParticles = [];
+        for (var i = 0; i < this.particles.length; i++) {
+            var newPos = this.move(this.particles[i]);
+            if (this.isParticleFree(newPos)) {
+                freeParticles.push(newPos);
+            } else {
+                this.stuckParticles.push(newPos);
+                this.grid[this.gridIndex(newPos.x, newPos.y)] = this.steps;
+            }
+        }
+        this.particles = freeParticles;
+        if (this.particles.length === 0) {
+            break;
+        }
+    }
+    var done = this.particles.length === 0;
+    if (done) {
+        this.duration = (performance.now() - this.startTime) / 1000;
+        console.log(
+            "Simulation finished in %s steps after %.2f seconds.",
+            this.steps, this.duration);
+    }
+    return done;
+};
+
+Simulation.prototype.paint = function (canvas) {
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var imagedata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imagedata.data;
+    var particle, index;
+    for (var i = 0; i < this.particles.length; i++) {
+        particle = this.particles[i];
+        index = this.gridIndex(particle.x, particle.y);
+        data[4 * index + 0] = 0x80;
+        data[4 * index + 1] = 0x80;
+        data[4 * index + 2] = 0x80;
+        data[4 * index + 3] = 0xff;
+    }
+    for (var j = 0; j < this.stuckParticles.length; j++) {
+        particle = this.stuckParticles[j];
+        index = this.gridIndex(particle.x, particle.y);
+        data[4 * index + 0] = 0x90;
+        data[4 * index + 1] = 0xff;
+        data[4 * index + 2] = 0x90;
+        data[4 * index + 3] = 0xd0;
+    }
+    ctx.putImageData(imagedata, 0, 0);
+};
